2bf62a6b07477a3bf5e7de195f1f3c36fd48fc33
andan04 <andan04@naver.com>
2021-04-10 16:50:26 +0900

    rpg, hunting

---
+++ 3D Hunting Game/Assets/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    float gravity = 9.81f;
+    float velocity;
+    public float jumpPower = 5;
+
+    public float playerMoveSpeed = 5;
+    private CharacterController controller;
+
+    private bool isJump = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        
+
+        Vector3 dir = new Vector3(h, 0, v);
+        dir.Normalize();
+        if (controller.collisionFlags == CollisionFlags.Below)
+        {
+            velocity = 0;
+            isJump = false;
+        }
+        if (Input.GetButtonDown("Jump") && !isJump)
+        {
+            isJump = true;
+            velocity = jumpPower;
+        }
+        velocity -= gravity * Time.deltaTime;
+
+        dir.y = velocity;
+
+        //dir = Camera.main.transform.TransformDirection(dir);
+
+        controller.Move(dir * Time.deltaTime * playerMoveSpeed);
+    }
+}
---
+++ 3D RPG Game/Assets/PickUp.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : It
+{
+    public Ittem item;
+
+    public override void Interact()
+    {
+        print("abc");
+        base.Interact();
+
+        Pick();
+    }
+
+    void Pick()
+    {
+        Debug.Log("use Item : " + item.name);
+
+        Destroy(gameObject);
+    }
+}
